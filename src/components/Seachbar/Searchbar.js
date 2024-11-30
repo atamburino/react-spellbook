3c9525bad07d724186cc4e950c7976bfd28a3951
Andrew Tamburino <57277787+atamburino@users.noreply.github.com>
2024-11-29 23:10:24 -0500

    class component created

---
+++ src/components/Seachbar/Searchbar.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Searchbar extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default Searchbar;
